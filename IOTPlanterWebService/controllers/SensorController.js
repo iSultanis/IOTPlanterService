6f41b9dbe058f3c3788752a9da9569bb6be54d97
iSultanis <i.sultanis@student.fontys.nl>
2019-11-15 12:51:07 +0100

    added time and id to controller

--- IOTPlanterWebService/controllers/SensorController.js
+++ IOTPlanterWebService/controllers/SensorController.js
@@ -3,8 +3,10 @@ module.exports = {
         let lux = req.body.lux;
         let temp = req.body.temp;
         let humidity = req.body.humidity;
+        let date = Date.now();
+        let id = 1;
 
         res.status(200);
-        res.send({lux, temp, humidity});
+        res.send({lux, temp, humidity, date, id});
     }
 };
