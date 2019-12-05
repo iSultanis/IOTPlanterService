03a88afc4eb5d36e78ddc4e264854b2e629a684e
Jeroen Jacobs <jacobs.jeroen97@gmail.com>
2019-12-05 15:58:34 +0100

    Check if body is filled with the correct data

--- IOTPlanterWebService/controllers/SensorController.js
+++ IOTPlanterWebService/controllers/SensorController.js
@@ -3,18 +3,39 @@ var sensorRepository = require('../repository/SensorRepository');
 
 module.exports = {
     postSensorData: function (req, res) {
-        let lux = req.body.lux;
-        let temp = req.body.temp;
-        let humidity = req.body.humidity;
-        let date = new Date().toLocaleString();
-        let id = 1;
+        let lux;
+        let temp;
+        let humidity;
         let plantId = 1;
+        let id = 1;
+        let date = new Date().toLocaleString();
+        if (req.body.lux != null) {
+            lux = req.body.lux;
+        } else {
+            res.status(400).send("Missing the value of lux");
+            return;
+        }
+        if (req.body.temp != null) {
+            temp = req.body.temp;
+        } else {
+            res.status(400).send("Missing the value of temp");
+            return;
+        }
+        if (req.body.humidity != null) {
+            humidity = req.body.humidity;
+        } else {
+            res.status(400).send("Missing the value of lux");
+            return;
+        }
+        req.body.plantid != null ? plantId = req.body.plantid : plantId = 1;
+
         let sensorData = new sensorModel(id, plantId, lux, temp, humidity, date);
 
         sensorRepository.postSensorData(sensorData);
         res.status(200);
         res.send({lux, temp, humidity, date, id});
     },
+
     getSensorData: async function (req, res) {
         let sensorData = await sensorRepository.getSensorData(req.params.id);
         console.log("data: \n" + sensorData);
